da69227a84e06f31fbe4e2243a891a7d6a38e9fc
santina-appalo <santinaappalo@gmail.com>
2024-10-17 01:52:47 +0530

    basic

--- medquest-frontend/src/App.js
+++ medquest-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
@@ -8,13 +8,13 @@ import Appointment from './components/Appointment';
 const App = () => {
     return (
         <Router>
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/appointments" component={Appointment} />
-                <Route path="/" exact component={Login} />
-            </Switch>
+            <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/appointments" element={<Appointment />} />
+                <Route path="/" element={<Login />} />
+            </Routes>
         </Router>
     );
 };
